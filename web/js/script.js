9c9fce50b5b1908139fc26153ac74df93ef51e4a
Dicky Dwi Putra <45366431+dickydwiputra17@users.noreply.github.com>
2020-07-27 03:35:01 +0700

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -1,26 +1,25 @@
 //event pada saat link di klik
-$('.page-scroll').on('click', function(e) {
-	//ambil isi href
-	var tujuan= $(this).attr('href');
-	console.log(tujuan)
+//	//ambil isi href
+	//var tujuan= $(this).attr('href');
+	
 	//tangkap elemen ybs
-	var elemenTujuan= $(tujuan);
+	//var elemenTujuan= $(tujuan);
 	
 	//pindahkan scroll
 	
-	$('body').animate({
-		scrollTop: elemenTujuan.offset().top - 50
-	},1250, 'easeInOutExpo');
+	//$('body').animate({
+		//scrollTop: elemenTujuan.offset().top - 50
+	//},1250, 'easeInOutExpo');
 
-	$('hr-excel').animate({
-	scrollTop: elemenTujuan.offset().top + 100
-	},1250, 'easeInOutExpo');
+	//$('hr-excel').animate({
+	//scrollTop: elemenTujuan.offset().top + 100
+	//},1250, 'easeInOutExpo');
 
-	$('hr-kontak').animate({
-	scrollTop: elemenTujuan.offset().top - 10
-	},1250, 'easeInOutExpo');
+	//$('hr-kontak').animate({
+	//scrollTop: elemenTujuan.offset().top - 10
+	//},1250, 'easeInOutExpo');
 	
-	e.preventDefault();
+	//e.preventDefault();
 	
 	 
-})
+//})
