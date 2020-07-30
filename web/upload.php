4dc0058b8176d0360f61a00bbc93cc34def12b57
zulmeika <51242831+zulmeika@users.noreply.github.com>
2020-07-30 10:48:06 +0700

    Update upload.php

--- web/upload.php
+++ web/upload.php
@@ -1,42 +1,27 @@
 <?php
-//require "convert2.php";
+
 if(isset($_POST["upload"])){
-    $file = $_FILES["berkas"];
-    $filename = $_FILES["berkas"]["name"];
-    $filedir = $_FILES["berkas"]["tmp_name"];
-    $imageFileType = strtolower(pathinfo($filedir,PATHINFO_EXTENSION));
-    //move_uploaded_file($filedir,"./uploads/");
-}
-$filechoosen = $filedir;
-$handle = fopen($filechoosen, "rb");
-$contents = fread($handle, filesize($filechoosen));
-$data = unpack('v*',$contents);
+    //baca multiple files
+    $countfile = 0;
+    foreach($_FILES["berkas"]["name"] as $filename){
+        $filedir = $_FILES["berkas"]["tmp_name"][$countfile];
+        $imageFileType = strtolower(pathinfo($filedir,PATHINFO_EXTENSION));
+        
+        //buka file binary
+        $filechoosen = $filedir;
+        $handle = fopen($filechoosen, "rb");
+        $contents = fread($handle, filesize($filechoosen));
+        $data = unpack('v*',$contents);
+        
 
-$filecsv = fopen("$filename.csv","w");
-fputcsv($filecsv, $data, "\n");
-fclose($handle);
-$fileopen = fopen("$filename.csv","r");
-$productData = array();
-$i = 1;
-while (($array = fgetcsv($fileopen, 0, ",")) !== FALSE) { 
-    $productData[] = (array('id'=>$i++, 'data'=> $array));    
-}
- 
-$json = json_encode($productData);
-file_put_contents("$filename.json", $json);
+        //mulai convert hasil fft ke json
+        $json = json_encode($data);
 
-fclose($filecsv);
-header("location:index3.php");   
-
-
-
-/*if(isset($_POST['upload']) && !empty($_FILES['berkas']['name'])) {
-    if(move_uploaded_file($_FILES["berkas"]["tmp_name"], $target_file)){
-        $statusMsg = "The file ".$file. " has been uploaded.";
-    }else{
-        $statusMsg = "Sorry, there was an error uploading your file.";
+        file_put_contents("$filename.json", $json);
+        fclose($handle);
+        fclose($filecsv);
     }
-}*/
+}
+header("location:index3.php");
 
-//echo $statusMsg;
 ?>
