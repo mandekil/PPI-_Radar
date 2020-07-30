848d65095dd0d7ef9063b4b05d7bd36c760f9122
zulmeika <51242831+zulmeika@users.noreply.github.com>
2020-07-30 10:56:50 +0700

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,301 @@
+<!doctype html>
+<html lang="en" id="home">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title> PPI Radar </title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body id="body">  
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top"style="background-color: #373A82; padding-top: 25px; padding-bottom: 25px; opacity: 100%; border-color: transparent; box-shadow: none;">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#home"><img src="img/8.png"  width="auto" height="50px" style="padding-top: 0px;"></a>
+        </div>    
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a style="color:#FFFFFF; font-weight:bold; font-size: 130%; "href="https://kirei.co.id/">PT. Kirei</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-left">
+            <li class="dropdown">
+              <a style="font-weight:bold; font-size: 130%; background-color: #373A82; color:#FFFFFF; "href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Layanan Informasi <span class="caret"></span></a>
+              <ul class="dropdown-menu"style="color:#FFFFFF; background-color: #373A82;">
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/2.png">Pesawat</a></li>
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/3.png">Free Space Langit</a></li>
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/4.png">Gedung Arhanud</a></li>
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/5.png">Gunung</a></li>
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/6.png">Lapangan</a></li>
+                <li><a style="color:#FFFFFF; background-color: #373A82;"href="img/7.png">Cimall</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav">
+            <li><a style="color:#FFFFFF; font-weight:bold; font-size: 130%;"href="#ppi" >PPI Radar</a></li>
+            <li><a style="color:#FFFFFF; font-weight:bold; font-size: 130%;"href="#contact1">Kontak</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="Search" class="btn btn-default">Search</button>
+          </form>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <!--akhir navbar-->
+
+
+    <!-- first -->
+    <section class="first" id="first">
+      <div class="thumbnail" style="padding: 0px; border:none;">
+        <img src="img/1.png" alt="PPI Radar">
+      </dir>
+    </section><!-- /first -->
+
+    <!-- layanan-informasi -->
+    <section class="layanan-informasi" id="layanan-informasi">
+      <div class="container">
+        <!-- row1 -->
+        <div class="row text-center">
+          <h3 style="font-weight:bold; font-size: 200%;">Layanan Informasi</h3>
+          <hr>
+        </div><!-- /row1 -->
+        
+        <!-- row2 -->
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Pesawat</h3>
+              <div class="thumbnail text-center tmb-no-border">
+                <img src="img/2.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Pesawat</h4>
+                <p><a href="img/2a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Free Space Langit</h3>
+              <div class="thumbnail text-center tmb-no-border">
+                <img src="img/3.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Free Space Langit</h4>
+                <p><a href="img/3a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Gedung Arhanud</h3>
+              <div class="thumbnail text-center tmb-no-border ">
+                <img src="img/4.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Gedung Arhanud</h4>
+                <p><a href="img/4a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+        </div><!-- /row2 -->
+
+        <!-- row3 -->
+        <div class="row row-bawah">
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Gunung</h3>
+              <div class="thumbnail text-center tmb-no-border">
+                <img src="img/5.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Gunung</h4>
+                <p><a href="img/5a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Lapangan</h3>
+              <div class="thumbnail text-center tmb-no-border">
+                <img src="img/6.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Lapangan</h4>
+                <p><a href="img/6a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="caption text-center">
+              <h3>Cimall</h3>
+              <div class="thumbnail text-center tmb-no-border">
+                <img src="img/7.png" width="auto" height="300">
+                <h4>Menampilkan informasi PPI Radar untuk Cimall</h4>
+                <p><a href="img/7a.png" class="btn btn-primary" role="button" target="_blank">Lihat</a></p>
+              </div>
+            </div>
+          </div>
+        </div><!-- /row3 -->
+      </div><!-- /container -->
+      <div style="padding-bottom: 50px; "class="ppi" id="ppi">
+      </div>
+    </section><!-- /konten -->
+
+
+    <!-- convert-excel -->
+    <hr style="margin-top: 15px; "id="hr-excel">
+    <section style="margin-top: 30px; "class="convert" id="convert">
+      <div class="container">
+        <!-- row1 -->
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2>PPI Radar</h2>
+            <hr>
+          </div>  
+        </div><!-- /row1 -->
+
+        <!-- row2 -->
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2">
+            <form>
+              <div class="from-group">
+                <label for="input">Input Data</label>
+                <i class="fa fa-upload"></i>
+                <!-- <input id="file" type="file" multiple="true"/> -->
+                <link rel="stylesheet" type="text/css" href="">
+                <div class="input-group btn-group">
+                  <input id="berkas" type="text" class="form-control" placeholder="Choose file">
+                  <span class="input-group-btn">
+                    <label class="btn btn-default">
+                      Browse <span><input type="file" style="display:none" name="file[]" id="file" multiple="true" /></span>
+                      <!-- <input type="file" style="display:none" name="berkas" id="berkas"> -->
+                    </label>
+                    <button type="submit" class="btn btn-primary" role="button" name=
+                    "upload">Submit</button>
+                  </span>
+                </div><!-- /input-group -->
+              </div>
+              <div class="from-group" style="margin-top: 20px;">
+                <label for="Jarak">Jarak:</label> 
+                <input type="text" id="Jarak" class="form-control" placeholder="Menampilkan Jarak Benda" style="background-color: #FFFFFF;" readonly=>
+              </div>
+              <div class="from-group" style="margin-top: 20px;">
+                <label for="Plot">Radar PPI Plot:</label>
+                <input type="text" id="Plot" class="form-control" placeholder="Menampilkan PPI Radar Plot" style="padding-top: 15px; padding-bottom: 270px; background-color: #FFFFFF;" readonly>
+                <div class="ok" id="ok"> 
+                  <ul class="radar">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+                <canvas id="rad-ppi"></canvas>
+              </div>
+              <div class="from-group" style="margin-top: 20px">
+                <label for="Grafik">Tampilan Grafik:</label>
+                <input type="text" id="Grafik" class="form-control" placeholder="Menampilkan Grafik"style="padding-top: 15px; padding-bottom: 270px; background-color: #FFFFFF;" readonly>
+                <canvas id="chart-ppi"></canvas>
+              </div>
+            </form>
+          </div>
+        </div><!-- /row2 -->
+      </div><!-- /container -->
+      <div style="padding-bottom: 50px; "class="contact1" id="contact1">
+      </div>
+    </section><!-- /pencarian -->
+
+
+    <!-- Kontak -->
+    <hr style="margin-top: 15px; "id="hr-kontak">
+    <section style="margin-top: 30px; "class="kontak" id="kontak">
+      <div class="container">
+        <!-- row1 -->
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2>Kontak</h2>
+            <hr>
+          </div>  
+        </div><!-- /row1 -->
+        <!-- row2 -->
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2">
+            <form>
+              <div class="from-group">
+                <label for="name">Nama</label>
+                <input type="text" id="nama" class="form-control" placeholder="masukan nama">
+              </div>
+              <div class="from-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" class="form-control" placeholder="masukan email">
+              </div>
+              <div class="from-group">
+                <label for="email">Pesan</label>
+                <input type="text" id="Pesan" class="form-control" placeholder="masukan pesan "style="padding-top: 15px; padding-bottom: 200px;">
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox"> Check me out
+                </label>
+              </div>
+              <button type="submit" class="btn btn-primary" role="button">Submit</button>
+            </form>
+          </div><!-- /col -->
+        </div><!-- /row2 -->
+      </div><!-- /container -->
+    </section><!-- /Kontak -->
+    
+    <!-- footer -->
+    <footer style="background-color: #373A82;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center" style="color:#FFFFFF;">
+            <h4>Copyright © 2020 | Built by Kirei</h4>
+            <h4>All Right Reserved</h4>
+            <h4>Version 1.0</h4>
+          </div>
+        </div><!-- /row -->
+      </div><!-- /container -->
+    </footer><!-- /footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="crossorigin="anonymous"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <!-- ChartJS -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3"></script>
+
+    <script src="js/script.js"></script>
+    <script src="js/chartppi.js"></script>
+  </body>
+</html>
