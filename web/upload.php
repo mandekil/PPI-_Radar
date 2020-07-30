818087453355c35ba9be42a32e3c818f0e61555a
zulmeika <51242831+zulmeika@users.noreply.github.com>
2020-07-30 12:02:54 +0700

    Update upload.php

--- web/upload.php
+++ web/upload.php
@@ -12,18 +12,58 @@ if(isset($_POST["upload"])){
         $handle = fopen($filechoosen, "rb");
         $contents = fread($handle, filesize($filechoosen));
         $data = unpack('v*',$contents);
+
+        $array = array ( $data);
+        $dataparse = is_array($array)? array_values($array): array();
         
 
         //mulai convert hasil fft ke json
-        $json = json_encode($data);
+        $arr = $dataparse;
+        //echo $arr[0].$arr[1].$arr[2].$arr[3];
 
-        for($i = 1; $i < count($_FILES["file"]["name"]); $i++){
+        $ar = array();
+        foreach ($arr as $key => $value) {
+            $new = array("id"=>$key, "nilai_ppi"=>$value);
+            array_push($ar, $new);
+        }
+
+        $json = json_encode($ar);
+        
+        for($i = 1; $i <= count($_FILES["file"]["name"]); $i++){
             file_put_contents("file_$i.json", $json);    
         }
+        
         fclose($handle);
-        fclose($filecsv);
+
     }
 }
-header("location:index3.php");
+header("location:index.php");
 
 ?>
+
+
+<script type="text/javascript" src="/js/fft.js"></script>
+<script>
+arrPHP2JS();
+function arrPHP2JS (){
+	var outArr = [];
+	var fft = new FFT();
+	var real = <?php echo json_encode($ar); ?>;
+	var imaginary = new Array(real.length); 
+	imaginary.fill(0);   
+	console.log(real)
+	console.log(imaginary)
+
+	//FFT
+	var fft = new FFT(); 
+	fft.calc(1, real, imaginary);
+	console.log(real);
+	console.log(imaginary);
+
+	for (var i=0; i<real.length; i++){
+		outArr.push(Math.sqrt(Math.pow(real[i],2) + Math.pow(imaginary[i],2)));
+	}
+	console.log(outArr);
+	return outArr;
+}
+</script>
